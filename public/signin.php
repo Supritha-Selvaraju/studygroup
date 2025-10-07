64adf67b2b2237b9da0ffd2ced8e9436bea46eb9
Supritha S <220701292@rajalakshmi.edu.in>
2025-10-08 01:45:12 +0530

    updated signin.php

--- public/signin.php
+++ public/signin.php
@@ -1,7 +1,11 @@
+
 <?php
 require 'config.php';
 header("Content-Type: application/json");
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $data = json_decode(file_get_contents("php://input"), true);
 
