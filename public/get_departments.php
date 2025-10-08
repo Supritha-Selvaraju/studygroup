f8cecbc3c2f05a08112aa79995dbf708b20e3e1c
Supritha S <220701292@rajalakshmi.edu.in>
2025-10-08 16:49:23 +0530

    Modified get_departments.php

--- public/get_departments.php
+++ public/get_departments.php
@@ -1,6 +1,8 @@
 <?php
 require 'config.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header("Content-Type: application/json");
 
 // Fetch all departments ordered alphabetically
