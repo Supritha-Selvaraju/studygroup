9d5b6f4b712c013ac8f61616f864cfd84ca9408c
Supritha S <220701292@rajalakshmi.edu.in>
2025-10-08 16:52:48 +0530

    Added test_db.php

--- public/test_db.php
+++ public/test_db.php
@@ -1,6 +1,13 @@
 <?php
-require 'config.php';
+$host = 'studygroup-mysql.mysql.database.azure.com';
+$db   = 'studygroup_db';
+$user = 'Supritha_S@studygroup-mysql';
+$pass = 'Julie@2004';
+
+$mysqli = new mysqli($host, $user, $pass, $db, 3306);
+
 if ($mysqli->connect_error) {
     die("DB Connect Error: " . $mysqli->connect_error);
 }
+
 echo "Connected successfully!";
